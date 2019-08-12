9f3e44f8ff0d4dccb7c8b2bbdad250f4dd39c812
Donatron <don.macarthur68@gmail.com>
2019-08-13 09:31:15 +1000

    Updates

--- original/src/js/models/Quote.js
+++
@@ -1,9 +0,0 @@
-export default class Quote {
-  constructor(quote, author, season, episode) {
-    this.quote = quote,
-    this.author = author,
-    this.season = season,
-    this.episode = episode
-  }
-
-}
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import quoteReducer from "./quoteReducer";
-import episodeReducer from "./episodeReducer";
-
-export default combineReducers({
-  quotes: quoteReducer,
-  episodes: episodeReducer
-});
